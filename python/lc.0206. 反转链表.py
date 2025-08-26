4c2f9ed766e587ab3b001f866018a2322cf0c896
kuronekonano <33979917+kuronekonano@users.noreply.github.com>
2025-08-27 02:45:49 +0800

    add 0025 0206

---
+++ python/lc.0206. 反转链表.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pre = None
+        while head != None:
+            last = head.next
+            head.next = pre
+            pre = head
+            head = last
+        return pre
